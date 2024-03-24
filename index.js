6faaf80481a5eed46ca0fe9832832e7d24f50a49
Varun Mohammed <57065342+VarunMohammed@users.noreply.github.com>
2024-03-24 08:39:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
+const AWS = require('aws-sdk');
 
 const mongoose = require('mongoose')
 const User = require('./user')
 
 dotenv.config({path: "./.env"});
 const app = express()
-const port = 1337
+const port = 8080
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'airmate.dev@gmail.com',
+        pass: 'bpzv zxdk uaxw dmnd'
+    }
+});
 
 mongoose.connect(`mongodb+srv://airmatedev:airmate.dev@airmate.dplynfb.mongodb.net/?retryWrites=true&w=majority&appName=airmate`)
 const db = mongoose.connection;
@@ -18,131 +28,113 @@ db.once('open', () => {
     console.log('Connected to MongoDB');
 })
 
-app.use(bodyParser.json());
+app.use(bodyParser.json());   
 
-app.post('/register', async (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const fullname = req.body.fullname;
+app.post('/send-email', (req,res) => {
+    const randomNum = Math.floor(Math.random() * 100000);
+    const otp = String(randomNum).padStart(5, '0');
 
-    const user = await User.findOne({ username });
-    if (user) {
-        res.status(409).json({ error: 'User already exists' });
-    } else {
-        const newUser = new User({ username, password, fullname });
-        await newUser.save();
-        res.status(201).json({ success: true });
-    }
-});
+    const mailOptions = {
+        from: 'airmate.dev@gmail.com',
+        to: 'varunmohammed2002@gmail.com',
+        subject: `${otp} : AirMate Password Reset OTP`,
+        text: `OTP to reset AirMate password is: ${otp}`
+    };
 
-app.post('/login', async (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const user = await User.findOne({ username });
-    if (user) {
-        res.status(200).json({ success: true, password: user.password });
-    } else {
-        res.status(404).json({ error: 'User not found' });
-    }
-});
-
-/*
-app.post('/forgot', (req, res) => {
-  const emailId = req.query.email;
-  //search if a mail  is present or not
-  let query = `SELECT * FROM users WHERE email='${emailId}'`;
-  pool.query(query ,(err, result)=>{
-      if(result != 0){
-          //send email to the received mailid with a random generated 5 digit code and store the code with emailid in the otp table
-          //var randomPassword=Math.random().toString(36).substring(8)+'@#$%';//generating the new password
-          const userOTP = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
-          var nodemailer = require('nodemailer') ;
-          
-          var transporter = nodemailer.createTransport({
-              service:'gmail',
-              auth:{
-                  user:"airmate.dev@gmail.com",
-                  pass:"airmate.dev@123"
-              }
-          });
-
-          var mailOptions={
-              from : 'airmate.dev@gmail.com',
-              to : emailId,
-              subject : "Your AirMate Password Reset Code!",
-              text : `This is your code :\n ${userOTP}\n\nThank you for using our services.\n\nBest Regards.`
-          }
-
-          console.log("Mail options are ",mailOptions);
-          transporter.sendMail(mailOptions,(error,info)=> {
-            if(error) {
-                res.status(500).send(`Error: ${error}`);
-            } else {
-                res.status(200).json({userOTP});
-            }
-          });
-          
-      }else{
-          res.end("Email Id not found");
-      }
-  });  
-  if (emailId && emailId === validEmail) {
-    // Email matches, return status code 200
-    res.status(200).json({ message: 'Email is valid' });
-  } else {
-    // Email doesn't match, return status code 400
-    res.status(400).json({ error: 'Invalid email' });
-  }
-});
-*/
-
-app.post('/forgot', (req, res) => {
-    const emailId = req.query.email;
-    const validEmail = "airmate.dev@gmail.com";
-    //search if a mail  is present or not
-    if (emailId && emailId === validEmail) {
-      // Email matches, return status code 200
-      res.status(200).json({ message: 'Email is valid' });
-    } else {
-      // Email doesn't match, return status code 400
-      res.status(400).json({ error: 'Invalid email' });
-    }
-  });
-
-app.post('/get-hash', (req,res) => {
-    const { password } = req.body;
-
-    bcrypt.hash(password, 10, (err, hashedValue) => {
-        if(err) {
-            print('Error:',err);
-            res.status(500).json({ err });
+    transporter.sendMail(mailOptions, (error,info) => {
+        if(error) {
+            console.log(error);
+            res.status(500).send('Error sending mail' + error);
         } else {
-            res.json({ hashedValue });
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent successfully').body(otp);
         }
     })
 })
 
-app.post('/update-password', (req,res) => {
-    const { username,newPassword } = req.body;
+app.post('/', (req, res) => {
+    res.status(200).json({ message: 'Hello' });
+})
 
-    if(!username || !newPassword) {
-        res.status(400).json({ error : "Please provide both a username and new password"});
-    }
-
-    const sql = 'UPDATE  users SET password = ? WHERE username = ?';
-    db.query(sql, [newPassword, username], (err, result) => {
-        if(err) {
-            return res.status(500).json({error: 'Internal Server Error'});
-        }
-
-        if(result.affectedRows === 0) {
-            return res.status(404).json({error:'No user found with that username.'});
-        }
-
-        res.status(200).json({message: 'Password updated'});
-    });
+app.post('/users', async (req, res) => {
+    const users = await User.find();
+    res.json(users);
 });
+
+app.post('/user/:id', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+});
+
+app.post('/user/:username', async (req, res) => {
+    const user = await User.findOne({ username: req.params.username });
+    res.json(user);
+});
+
+app.post('/user', async (req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.json(user);
+});
+
+app.delete('/user/:id', async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.json(user);
+});
+
+app.put('/user/:id', async (req, res) => {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.json(user);
+});
+
+app.post('/register', async (req,res) => {
+    const { email, password } = req.body;
+
+    const params = {
+        ClientId: process.env.CLIENT_ID,
+        Password: password,
+        Username: email,
+        UserAttributes: [
+            {
+                Name: 'email',
+                Value: email
+            }
+        ]
+    };
+
+    try {
+        await CognitoIdentity.signUp(params).promise();
+        res.status(200).json({ message: 'User registered successfully' });
+    } catch (err) {
+        if (err.code === "UsernameExistsException") {
+            res.status(409).json({ message: 'User already exists' });
+        } else {
+            res.status(500).json({ message: `Failed to register user: ${err}` });
+        }
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    
+    const user = await User.findOne({ email, password });
+    if(user) {
+        res.status(200).json({ message: 'User logged in successfully' });
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+});
+
+app.post('/forgotpassword', async (req,res) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+    if(user) {
+        //User Exist
+    } else {
+        //Failed 
+    }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
